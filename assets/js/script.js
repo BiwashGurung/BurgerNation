d331ea32578ae7e4f9557130adae7d5d8adc276d
BiwashGurung <varungrg10@gmail.com>
2023-08-29 08:48:34 +0545

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-window.addEventListener('load', () => {
-    const preload = document.querySelector('.preload');
-    preload.classList.remove('load');
-  });
+'use strict';
+
+const preload = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function () {
+  preload.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
+// window.addEventListener('load', () => {
+//     const preload = document.querySelector('.preload');
+//     preload.classList.remove('load');
+  
+//     // Scroll to the target section after 3 seconds
+//     setTimeout(() => {
+//       const targetSection = document.getElementById('target-section');
+//       targetSection.scrollIntoView({ behavior: 'smooth' });
+//     }, 3000); // 3000 milliseconds = 3 seconds
+//   });
